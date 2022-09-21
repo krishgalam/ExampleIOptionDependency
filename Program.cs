78beea3531e0b5727c15d3cea39fb97c651822de
gitName <a@b.c>
2022-09-21 08:21:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleIOptionDependency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
